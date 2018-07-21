ef96a35622388e96be11c125235147cccf42de10
VikVelev <viktorvelev8@gmail.com>
2018-07-21 15:30:59 +0300

    Added great research item

--- qub-spa/src/components/MainFeed.jsx
+++ qub-spa/src/components/MainFeed.jsx
@@ -2,13 +2,14 @@ import React, { Component } from 'react'
 import Research from './items/ResearchItem'
 import NavBar from './NavBar'
 import repo from '../globals/Repo'
+import { observer } from 'mobx-react'
 
+@observer
 export default class MainFeed extends Component {
     render() {
         return(
             <div className="main-container">
-                {/* {repo.researches.map((data, i) => <Research key={i} data={data}/>)} */}
-                <Research/>
+                {repo.researches.map((data) => <Research {...data}/>)}
             </div>
         )
     }
