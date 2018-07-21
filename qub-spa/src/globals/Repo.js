c7ea17b7510cc86969b5de7f75ede0c9fc442d8e
alex4o <mrabonin@gmail.com>
2018-07-21 12:14:26 +0100

    Added paper url

--- qub-spa/src/globals/Repo.js
+++ qub-spa/src/globals/Repo.js
@@ -8,8 +8,8 @@ let res = new Research();
 
 res.id = "0xFAD34F43434485684586"
 res.isLocked = false
-res.paperURL = "url.paper"
-res.reproducedURL = "reproducted://asdfasd"
+res.paperURL = "https://arxiv.org/pdf/astro-ph/9806002.pdf"
+res.reproducedURL = "https://arxiv.org/pdf/astro-ph/9806002.pdf"
 res.reproducer = "Ivan petrov"
 res.researcher = "Georgi Dimitrov"
 res.stakedAmount = 200
