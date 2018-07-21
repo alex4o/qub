2e10ab4186c1fb334ab0973a6fa6e284aa562afc
VikVelev <viktorvelev8@gmail.com>
2018-07-21 12:14:40 +0300

    Added mainfeed

---
+++ qub-spa/src/components/MainFeed.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class MainFeed extends Component {
+    render() {
+        return(
+            <div>
+                kur
+            </div>
+
+        )
+    }
+}
