cc254821425b72f544197a34e52755536701d3bb
alex4o <mrabonin@gmail.com>
2018-07-21 21:39:14 +0100

    empty?

--- qub-spa/src/components/AddResearch.jsx
+++ qub-spa/src/components/AddResearch.jsx
@@ -32,8 +32,12 @@ export default class AddResearch extends Component {
                 <Segment className="fundModal vote-segment" >
                     <h2 className="vote-title">Add a new research</h2>
                     <Input name="title" type="text" onChange={this.handleChange} placeholder="Enter your research title"/>
+<<<<<<< HEAD
                     <Input name="paperUrl" type="text" onChange={this.handleChange} placeholder="Enter your paper url"/>
                     <Message info>NOTICE: Submitting a research paper requires a fee.</Message>                
+=======
+                    <Input name="paperUrl" type="text" onChange={this.handleChange} placeholder="Enter your paper url"/>                    
+>>>>>>> empty?
                     <Button inverted color="green" className="submit" onClick={this.submit.bind(this)}>Add</Button>
                 </Segment>
             </Modal>
