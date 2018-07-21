a0ba0bbb3f57fe6affc40ccd3e4fb57526d96419
alex4o <mrabonin@gmail.com>
2018-07-21 12:18:10 +0100

    Added some data

--- qub-spa/src/globals/Repo.js
+++ qub-spa/src/globals/Repo.js
@@ -17,6 +17,35 @@ res.stakers = 4
 res.state = 1
 res.votes = []
 
+
+
+res = new Research();
+
+res.id = "0xFAD34CCD3434485684586"
+res.isLocked = true
+res.paperURL = "https://arxiv.org/pdf/1707.07702.pdf"
+res.reproducedURL = "https://arxiv.org/pdf/1707.07702.pdf"
+res.reproducer = "Steven Hawking"
+res.researcher = "Georgi Dimitrov"
+res.stakedAmount = 200
+res.stakers = 4
+res.state = 1
+res.votes = []
+
+
+res = new Research();
+
+res.id = "0xFAD34CCD3434485684586"
+res.isLocked = true
+res.paperURL = "https://arxiv.org/pdf/1701.08110.pdf"
+res.reproducedURL = ""
+res.reproducer = "Steven Hawking"
+res.researcher = ""
+res.stakedAmount = 800
+res.stakers = 6
+res.state = 1
+res.votes = []
+
 repo.researches.push(res)
 
 export default repo
