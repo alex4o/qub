fb72e36286db0b82029361864dc10bc352b9252f
VikVelev <viktorvelev8@gmail.com>
2018-07-21 11:56:14 +0300

    Added main file structure

--- qub-spa/src/App.js
+++ qub-spa/src/App.js
@@ -3,12 +3,13 @@ import React, {
 } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
 
 class App extends Component {
 	render() {
 		return(
 			<div className="App">
-				kur
+
 			</div>
 		);
 	}
