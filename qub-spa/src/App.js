74341f4c16dea297b1861dcef73f3cb4f825da95
VikVelev <viktorvelev8@gmail.com>
2018-07-21 12:11:08 +0300

    Added imports

--- qub-spa/src/App.js
+++ qub-spa/src/App.js
@@ -4,13 +4,17 @@ import React, {
 import logo from './logo.svg';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router } from "react-router-dom";
+import MainFeed from './components/MainFeed'
+
 
 class App extends Component {
 	render() {
 		return(
-			<div className="App">
-
-			</div>
+			<Router className="App">
+				<MainFeed/>
+			</Router>
 		);
 	}
 }
