55ca56a7de343cf9d4e3fab247a474b7e50ed6b1
VikVelev <viktorvelev8@gmail.com>
2018-07-21 20:19:46 +0300

    Added add research modal

--- qub-spa/src/components/AddResearch.jsx
+++ qub-spa/src/components/AddResearch.jsx
@@ -1,12 +1,35 @@
 import React, { Component } from 'react'
-import { Modal, Segment } from 'semantic-ui-react'
+import { Modal, Segment, Input, Button } from 'semantic-ui-react'
+
 
 export default class AddResearch extends Component {
+
+    constructor(props){
+        super(props)
+        
+        this.state = {
+            title: "",
+            paperUrl: "",
+        }
+    }
+
+    handleChange = (e, { name, value }) => {
+		this.setState({ [name]: value })
+	}
+
+    submit() {
+        console.log("submit")
+        //this.state is the argument
+    }
+
     render(){
         return(
-            <Modal trigger={this.props.trigger} className="add-research-modal" closeIcon>
-                <Segment>
-
+            <Modal trigger={this.props.trigger} className="add-research-modal vote-modal" closeIcon>
+                <Segment className="fundModal vote-segment" >
+                    <h2 className="vote-title">Add a new research</h2>
+                    <Input name="title" type="text" onChange={this.handleChange} placeholder="Enter your research title"/>
+                    <Input name="paperUrl" type="text" onChange={this.handleChange} placeholder="Enter your paper url"/>                    
+                    <Button inverted color="green" className="submit" onClick={this.submit}>Add</Button>
                 </Segment>
             </Modal>
         )
