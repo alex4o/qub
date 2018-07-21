c36043e6d8de17acb6e52571b575e38efae943e6
zvezdin <git@zvezd.in>
2018-07-21 21:21:29 +0300

    Add unit test for registrations

--- blockchain/truffle/test/science.js
+++ blockchain/truffle/test/science.js
@@ -13,14 +13,27 @@ contract('Science', function(accounts) {
 
 	let publishFee = web3.toWei(10, "ether");
 
+	it("should register", async () => {
+		let sci = await Science.deployed();
+		
+		await sci.register(publisherOrcid, {from: publisher});
+		let orcid = await sci.getMyOrcid({from: publisher});
+
+		assert.equal(orcid, publisherOrcid);
+
+		let orcid2 = await sci.addressToOrcid(publisher);
+		
+		assert.equal(orcid2, publisherOrcid);
+	});
+
 	it("should publish research", async () => {
 		let sci = await Science.deployed();
-		console.log("test...", publishFee, publisherOrcid, paperUrl, title, publisher);
-		await sci.publishResearch(publisherOrcid, paperUrl, title, {from: publisher, value: publishFee})
+		await sci.publishResearch(paperUrl, title, {from: publisher, value: publishFee})
 		console.log("ok...");
 		let researches = await sci.getResearchKeys();
 		assert.equal(researches.length, 1);
-		let research = await sci.researches(researches[0]);
+		console.log("Res len is 1", researches);
+		let research = await sci.researches("123");
 
 
 		//assert.equals(research[0])
