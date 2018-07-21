a605754cb4b7d3cca17fd7444d68a1adae246871
alex4o <mrabonin@gmail.com>
2018-07-21 12:06:02 +0100

    added some data

--- qub-spa/src/states/ResearchList.js
+++ qub-spa/src/states/ResearchList.js
@@ -1,7 +1,6 @@
 import { observable } from "mobx"
 
 export default class ResearchList {
-    @observable keys = []
-    @observable researches;
+    @observable researches = [];
     @observable addressToOrcid;
 }
