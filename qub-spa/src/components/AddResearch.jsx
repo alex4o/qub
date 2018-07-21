45c9753f84ac753832d4b6356a698fd4701f47ec
alex4o <mrabonin@gmail.com>
2018-07-21 21:36:14 +0100

    fixed some small things

--- qub-spa/src/components/AddResearch.jsx
+++ qub-spa/src/components/AddResearch.jsx
@@ -31,14 +31,9 @@ export default class AddResearch extends Component {
                 <Segment className="fundModal vote-segment" >
                     <h2 className="vote-title">Add a new research</h2>
                     <Input name="title" type="text" onChange={this.handleChange} placeholder="Enter your research title"/>
-<<<<<<< HEAD
                     <Input name="paperUrl" type="text" onChange={this.handleChange} placeholder="Enter your paper url"/>
                     <Message info>NOTICE: Submitting a research paper requires a fee.</Message>                
                     <Button inverted color="green" className="submit" onClick={this.submit}>Add</Button>
-=======
-                    <Input name="paperUrl" type="text" onChange={this.handleChange} placeholder="Enter your paper url"/>                    
-                    <Button inverted color="green" className="submit" onClick={this.submit.bind(this)}>Add</Button>
->>>>>>> 94a690de804da0e40b6f01a20fc03aac32afbd3a
                 </Segment>
             </Modal>
         )
